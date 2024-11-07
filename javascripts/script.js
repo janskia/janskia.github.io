b14e5c502c0b0d3557f25dcd3015f60140b8c587
janskia <janski.adam@gmail.com>
2024-11-07 22:05:25 +0100

    root on processing page

--- javascripts/script.js
+++ javascripts/script.js
@@ -3,5 +3,5 @@ function nextImage(obj, max){
     var newIndex = (Number(splittedImageName[splittedImageName.length - 2]) + 1) % max;
     splittedImageName[splittedImageName.length - 2] = newIndex;
     obj.children[0].src = splittedImageName.join('.');
-    obj.children[1].innerHTML = (newIndex+1) + "/" + max;
+    //obj.children[1].innerHTML = (newIndex+1) + "/" + max;
 }
