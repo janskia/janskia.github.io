1af58550a5ab7eb9790b71dbb013de870e7047e9
Adam Janski <janski.adam@gmail.com>
2025-11-14 15:55:52 +0100

    fullscreen

--- live/flowers.js
+++ live/flowers.js
@@ -20,6 +20,7 @@ function setup() {
 }
 
 function mousePressed(){
+  openFullscreen();
   drawFlower();
 }
 
@@ -69,4 +70,16 @@ function prepareShape(){
     arr.push([xOffset + random(xRandom), yOffset + random(yRandom)]);
   }
   return arr;
+}
+
+/* View in fullscreen */
+function openFullscreen() {
+  var elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
 }
